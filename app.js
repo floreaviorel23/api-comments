4ee54bd05d7ab5c4edf75dfa8b0a33aeb1bd493e
floreaviorel23 <floreaviorel23@yahoo.com>
2022-02-08 23:24:29 +0200

    implemented pug

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
 const express = require('express');
 const app = express();
 const PORT = 3000;
+const path = require('path');
 app.use(express.json());
 
-
 let comments = [];
+let toSend = {};
 
+// - - - - - - - - - - Load View Engine - - - - - - - - -
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+// - - - - - - - - - - Set public path - - - - - - - - -
+app.set(express.static(path.join(__dirname, 'public')));
+app.use('/css', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css')))
+
+
+
+
+// - - - - - - - - - - Database config - - - - - - - - -
 let Connection = require('tedious').Connection;
 let config = {
     server: "192.168.0.102", // or "localhost"
@@ -24,32 +37,131 @@ let config = {
 };
 let connection = new Connection(config);
 
-
-
-
-
-
+// - - - - - - - - - - Database connection - - - - - - - - -
 function dbConnection() {
     connection.connect((err) => {
         if (err) {
-            console.log('Error: ', err)
+            console.log('Error dbConnection : ', err)
         }
         else {
             console.log("Connected to db : " + config.options.database);
         }
     });
 }
-
 dbConnection();
 
 
 
+// - - - - - - - - - - Express routes - - - - - - - - -
+
+app.listen(PORT, () => {
+    console.log("It's alive on port : " + PORT);
+});
+
+
+app.get("/", async (req, res) => {
+    console.log("GET Request from /");
+    try {
+        await getAllComments();
+        res.status(200);
+        res.render('index', toSend);
+        //res.send(`${JSON.stringify(comments)}`);
+    }
+    catch (err) {
+        console.log(err);
+        res.status(500);
+        res.send("Get request failed");
+    }
+});
+
+
+app.get("/:uuid", async (req, res) => {
+    console.log("GET Request from /uuid");
+    const uuid = req.params.uuid;
+
+    try {
+        const result = await getComment(uuid);
+        if (Object.keys(result).length === 0 && result.constructor === Object) {
+            res.status(400);
+            res.send("ID not found");
+        } else {
+            res.status(200);
+            res.send(`${JSON.stringify(result)}`);
+        }
+    }
+    catch (err) {
+        console.log(err);
+        res.status(500);
+        res.send("GET request failed");
+    }
+
+});
+
+
+app.post("/", async (req, res) => {
+    console.log("POST Request from /");
+    const [avatar, message, author] = [req.body.avatar, req.body.message, req.body.author];
+
+    if (!avatar || !message || !author) {
+        res.status(418).send("Request missing something");
+    }
+    else {
+        let com = { avatar, message, author };
+        try {
+            await insertNewComment(com);
+            res.status(200);
+            res.send("Comment successfuly added");
+        }
+        catch (err) {
+            console.log("error db : " + err);
+            res.status(500).send("POST Request failed");
+        }
+    }
+});
 
 
 
+app.delete("/:uuid", async (req, res) => {
+    console.log("DELETE Request from /uuid");
+    const uuid = req.params.uuid;
+
+    try {
+        const result = await removeComment(uuid);
+        res.status(200);
+        res.send("Deleted from db");
+    }
+    catch (errorMessage) {
+        console.log(errorMessage);
+        res.status(418);
+        res.send("Nu a mers");
+    }
+});
 
 
+app.put("/:uuid", async (req, res) => {
+    console.log("GET Request from /id");
+    const uuid = req.params.uuid;
+    [avatar, message, author] = [req.body.avatar, req.body.message, req.body.author];
+    console.log(avatar, message, author);
 
+    if ((avatar == '' && message == '' && author == '') || (!avatar && !message && !author)) {
+        res.status(400);
+        res.send("Request missing something");
+    }
+    else {
+        try {
+            const result = await updateComment(uuid, avatar, message, author);
+            res.status(200);
+            res.send('Updated with success');
+        }
+        catch (err) {
+            console.log(err);
+            res.status(500).send("PUT Request failed")
+        }
+    }
+});
+
+// - - - - - - - - - - Select all comments from db - - - - - - - - -
 function getAllComments() {
     const prom = new Promise((resolve, reject) => {
         comments = [];
@@ -61,33 +173,30 @@ function getAllComments() {
         });
         dbrequest.on('requestCompleted', () => {
             //console.log("Request completed");
+            toSend.comments = comments;
+            //console.log(toSend);
             resolve("success");
         });
 
+        dbrequest.on('error', (err) => {
+            reject(err);
+        });
         connection.execSql(dbrequest);
     });
     return prom;
 }
 
-
 function makeSelectRequest(sql) {
     let Request = require('tedious').Request;
     const dbrequest = new Request(sql, (err, rowCount) => {
         if (err) {
-            console.log("err get: ", err);
-        }
-        else {
-            //console.log("no err get");
-            //console.log("Row count : " + rowCount);
+            console.log("err GET");
         }
     });
     return dbrequest;
 }
 
-
-
-
-
+// - - - - - - - - - - Select a single comment from db - - - - - - - - -
 function getComment(uuid) {
     const prom = new Promise((resolve, reject) => {
 
@@ -98,7 +207,7 @@ function getComment(uuid) {
         dbrequest.on('row', (columns) => {
             const [uuid, avatar, message, author, createdAt, editedAt] =
                 [columns[1].value, columns[2].value, columns[3].value, columns[4].value, columns[5].value, columns[6].value];
-            com = { avatar, message, author, createdAt, editedAt };
+            com = { uuid, avatar, message, author, createdAt, editedAt };
         });
 
         dbrequest.on('requestCompleted', () => {
@@ -111,48 +220,30 @@ function getComment(uuid) {
     return prom;
 }
 
-
-
-
-
-
-
+// - - - - - - - - - - Push to array the values obtained from select - - - - - - - - -
 function pushCommentToArray(columns) {
-    const [avatar, message, author, createdAt, editedAt] =
-        [columns[2].value, columns[3].value, columns[4].value, columns[5].value, columns[6].value];
-    let com = { avatar, message, author, createdAt, editedAt };
+    const [uuid, avatar, message, author, createdAt, editedAt] =
+        [columns[1].value, columns[2].value, columns[3].value, columns[4].value, columns[5].value, columns[6].value];
 
+    let com = { uuid, avatar, message, author, createdAt, editedAt };
     comments.push(com);
 }
 
-
-
-
-
-
-
-
-
-
-
+// - - - - - - - - - - Insert comment into db - - - - - - - - -
 function insertNewComment(comment) {
     const prom = new Promise((resolve, reject) => {
 
-        let sql = `exec InsertNewComment "${comment.avatar}", "${comment.message}", "${comment.author}"`;
         let Request = require('tedious').Request;
+        let sql = `exec InsertNewComment "${comment.avatar}", "${comment.message}", "${comment.author}"`;
+
         const dbrequest = new Request(sql, (err, rowCount) => {
             if (err) {
-                console.log("err post: ", err);
-            }
-            else {
-                //console.log("no err");
-                //console.log("Row count : " + rowCount);
+                reject(err);
+                //console.log("err POST : ", err);
             }
         });
 
-
         dbrequest.on('requestCompleted', () => {
-            //console.log("Request completed post");
             resolve("success");
         });
 
@@ -162,23 +253,19 @@ function insertNewComment(comment) {
 }
 
 
-
+// - - - - - - - - - - Delete comment from db - - - - - - - - -
 function removeComment(uuid) {
     const prom = new Promise((resolve, reject) => {
 
-        let sql = `exec DeleteComment "${uuid}"`;
         let Request = require('tedious').Request;
+        let sql = `exec DeleteComment "${uuid}"`;
+
         const dbrequest = new Request(sql, (err, rowCount) => {
             if (err) {
-                //reject("failed removeComment");
-                console.log("err delete: ", err);
-            }
-            else {
-                //console.log("no err");
-                //console.log("Row count : " + rowCount);
+                reject("failed removeComment");
+                //console.log("err delete: ", err);
             }
         });
-
 
         dbrequest.on('requestCompleted', () => {
             //console.log("Request completed post");
@@ -190,147 +277,55 @@ function removeComment(uuid) {
     return prom;
 }
 
+// - - - - - - - - - - Update comment from db - - - - - - - - -
+function updateComment(uuid, avatar, message, author) {
+    const prom = new Promise((resolve, reject) => {
 
+        let Request = require('tedious').Request;
+        if (!avatar || avatar == '')
+            avatar = 'NULL'
+        else
+            avatar = `"${avatar}"`;
 
+        if (!message || message == '')
+            message = 'NULL'
+        else
+            message = `"${message}"`;
 
+        if (!author || author == '')
+            author = 'NULL'
+        else
+            author = `"${author}"`;
 
+        let sql = `exec UpdateComment "${uuid}", ${avatar}, ${message}, ${author}`;
 
+        const dbrequest = new Request(sql, (err, rowCount) => {
+            if (err) {
+                reject("failed updateComment");
+                //console.log("err delete: ", err);
+            }
+        });
 
+        dbrequest.on('requestCompleted', () => {
+            //console.log("Request completed post");
+            resolve("success");
+        });
 
-//const fs = require('fs');
-
-// const data = fs.readFileSync('./fakedb/comments.json',
-//     { encoding: 'utf8' });
-// comments = JSON.parse(data);
-
-
-// fs.readFile('./fakedb/comments.json', (err, data) => {   // Asynchronous file read
-//     comments = JSON.parse(data);
-//     console.log(comments);        // Won't be shown unless you use the synchronous function
-// })
-
-//comment
-// {
-//     avatar: emoji
-//     message: text
-//     author: text
-//     createdAt: Date  -- Not given by user
-//     editedAt: Date   -- Not given by user
-//   }
-
-
-
-
-
-
-
-
-
-app.listen(PORT, () => {
-    console.log("It's alive on port : " + PORT);
-});
-
-
-
-
-
-app.get("/", async (req, res) => {
-    console.log("GET Request from /");
-    await getAllComments();
-    res.status(200);
-    res.send(`${JSON.stringify(comments)}`);
-});
-
-
-
-
-app.get("/:uuid", async (req, res) => {
-    console.log("GET Request from /uuid");
-    const uuid = req.params.uuid;
-    const result = await getComment(uuid);
-
-    if (Object.keys(result).length === 0 && result.constructor === Object) {
-        res.status(400);
-        res.send("ID not found");
-    } else {
-        res.status(200);
-        res.send(`${JSON.stringify(result)}`);
-    }
-
-});
-
-
-
-
-
-
-
-app.post("/", async (req, res) => {
-    console.log("POST Request from /");
-    const [avatar, message, author] = [req.body.avatar, req.body.message, req.body.author];
-
-    if (!avatar || !message || !author) {
-        res.status(418).send("Nu a mers");
-    }
-    else {
-        let com = { avatar, message, author };
-        await insertNewComment(com);
-        res.status(200);
-        res.send("Comment successfuly added");
-    }
-});
-
-
-
-
-
-app.delete("/:uuid", async (req, res) => {
-    console.log("DELETE Request from /uuid");
-    const uuid = req.params.uuid;
-
-    const result = await removeComment(uuid);
-    res.status(200);
-    res.send("Deleted from db");
-
-});
-
-
-
-
-
-
-app.put("/:id", (req, res) => {
-    console.log("GET Request from /id");
-    const id = req.params.id;
-    if (!isNaN(id) && id < comments.length && id >= 0) {
-        const avatar2 = req.body.avatar;
-        const message2 = req.body.message;
-        const author2 = req.body.author;
-        updateComment(id, avatar2, message2, author2);
-
-        res.status(200);
-        res.send(`${JSON.stringify(comments[id])}`);
-    }
-    else {
-        res.status(400);
-        res.send("Oh no it do be not working");
-    }
-});
-
-
-
-
-
-function updateComment(id, avatar2, message2, author2) {
-    if (avatar2 && avatar2 != '') {
-        comments[id].avatar = avatar2;
-    }
-    if (message2 && message2 != '') {
-        comments[id].message = message2;
-    }
-    if (author2 && author2 != '') {
-        comments[id].author = author2;
-    }
-    comments[id].editedAt = new Date();
+        connection.execSql(dbrequest);
+    });
+    return prom;
 }
 
+// - - - - - - - - - - Using a text file instead of a database - - - - - - - - -
+/*
+const fs = require('fs');
+
+const data = fs.readFileSync('./fakedb/comments.json',
+    { encoding: 'utf8' });
+comments = JSON.parse(data);
+
+fs.readFile('./fakedb/comments.json', (err, data) => {   // Asynchronous file read
+    comments = JSON.parse(data);
+    console.log(comments);        // Won't be shown unless you use the synchronous function
+})
+*/
